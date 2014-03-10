452a17312d0b3cc8ddc8707e3eddc62d874dc126
Evan Paul <evanwpaul@gmail.com>
2014-03-10 19:15:31 -0400

    changing constants back to openshift settings

--- php/inc/constants.inc.php
+++ php/inc/constants.inc.php
@@ -3,7 +3,7 @@
     // Database credentials
 	//below were entered in at 11:17 pm 2/4/14 based on openshift MySQL cartridge credentials
 	//phpmyadmin URL = https://php-epsilon670.rhcloud.com/phpmyadmin/
-    /*
+    
     define('DB_HOST', '127.11.113.2');
     define('DB_USER', 'adminEwNp6ZY');
     define('DB_PASS', '22gIfbwiDwh7');
@@ -12,12 +12,11 @@
     //Define DOMAIN and BRAND constants for site:
 	define('DOMAIN', 'http://www.poolski.com/');
 	define('BRAND_NAME', 'Poolski');
-    */
-
+    
 
     //Below credentials are for localhost
     
-    
+    /*
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
     define('DB_PASS', 'william88');
@@ -26,7 +25,7 @@
 	//Define DOMAIN and BRAND constants for site:
 	define('DOMAIN', 'http://localhost/openshift_clone/php/php/');
 	define('BRAND_NAME', 'Poolski');
-	
+	*/
 
 ?>
 
