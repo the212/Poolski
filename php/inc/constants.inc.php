42db47c545f416c87e883d0f346d664827fd514b
Evan Paul <evanwpaul@gmail.com>
2014-09-20 12:00:15 -0700

    committing constants file with remote constants enabled since i forgot to do that for the last commit with new pool series stuff

--- php/inc/constants.inc.php
+++ php/inc/constants.inc.php
@@ -3,7 +3,7 @@
     // Database credentials
 	//below were entered in at 11:17 pm 2/4/14 based on openshift MySQL cartridge credentials
 	//phpmyadmin URL = https://php-epsilon670.rhcloud.com/phpmyadmin/
-    /*
+    
     define('DB_HOST', '127.11.113.2');
     define('DB_USER', 'adminEwNp6ZY');
     define('DB_PASS', '22gIfbwiDwh7');
@@ -14,10 +14,10 @@
 	define('BRAND_NAME', 'Poolski');
 
     define('FRIEND', 'Friendski');
-    */
+    
     
     //Below credentials are for localhost
-    
+    /*
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
     define('DB_PASS', 'william88');
@@ -28,5 +28,5 @@
 	define('BRAND_NAME', 'Poolski');
     
     define('FRIEND', 'Friendski');
-	
+	*/
 ?>
